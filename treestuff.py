72098da7e4c2e8d9c1514f62b31e6c8a0f375805
Andrew Barnert <abarnert@yahoo.com>
2016-02-17 11:41:36 -0800

    First version

---
+++ treestuff.py
@@ -0,0 +1,73 @@
+import collections
+from operator import attrgetter, itemgetter
+
+# Most tree algorithms can be written as higher-order functions.
+# For example, any top-down traversal just needs a function to go
+# from a node to an iterable of its children. And that's true
+# whether you write the traversal recusively:
+
+def postorder(node, children_func):
+    for child in children_func(node):
+        yield from postorder(child, children_func)
+    yield node
+
+# ... or iteratively:
+        
+def preorder(node, children_func):
+    s = [node]
+    while s:
+        node = s.pop()
+        yield node
+        s.extend(reversed(list(children_func(node))))
+
+# ... and of course it works just as well for BFS as fro DFS:
+
+def levelorder(node, children_func):
+    q = collections.deque([node])
+    while q:
+        node = q.popleft()
+        yield node
+        q.extend(children_func(node))
+
+# Doing things this way means we can use completely different
+# structures and APIs for trees, depending on what we need, and
+# use the same generic functions.
+
+# For example, this tree is just a tuple of tuples:
+simpletree = (1, ((2, ((4, ()), (5, ()))), (3, ())))
+
+# Its "children" function is just lambda node: node[1]--or, more simply:
+for node in levelorder(simpletree, itemgetter(1)):
+    print(node[0], end=' ')
+print()
+
+# But the same functions work on something more complicated:
+from xml.etree import ElementTree as ET
+xdoc = ET.fromstring("""
+<node value="1">
+    <node value="2">
+        <node value="4"/>
+        <node value="5"/>
+    </node>
+    <node value="3"/>
+</node>
+""")
+for node in levelorder(xdoc, iter):
+    print(node.get('value'), end=' ')
+print()
+
+# You can even write a higher-order printing function that takes
+# the traversal function as an argument:
+
+def print_tree(node, value_func, children_func, traversal_func):
+    for node in traversal_func(node, children_func):
+        print(value_func(node), end=' ')
+    print()
+
+print_tree(simpletree, itemgetter(0), itemgetter(1), preorder)
+print_tree(simpletree, itemgetter(0), itemgetter(1), postorder)
+print_tree(simpletree, itemgetter(0), itemgetter(1), levelorder)
+
+print_tree(xdoc, lambda node: node.get('value'), iter, preorder)
+print_tree(xdoc, lambda node: node.get('value'), iter, postorder)
+print_tree(xdoc, lambda node: node.get('value'), iter, levelorder)
